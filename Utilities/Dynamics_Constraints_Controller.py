bd985c69e58d84ff4e5d2d0972fe58e217e67647
Mohammad <msramada@eng.ucsd.edu>
2023-06-29 00:00:43 -0700

    Adjusting gains and params

--- Utilities/Dynamics_Constraints_Controller.py
+++ Utilities/Dynamics_Constraints_Controller.py
@@ -4,16 +4,18 @@ import cvxpy as cp
 
 rx = 3
 ru = 1
-ry = 2
-Q = 0.1 * np.diag(np.ones(rx,))
-R = 0.2 * np.diag(np.ones(ry,))
-V = 10
+ry = 3
+Q = 0.2 * np.diag(np.ones(rx,))
+Q[2,2] = 0.1
+R = 0.1 * np.diag(np.ones(ry,))
+R[2,2] = 0.1
+V = 5
 def stateDynamics(x, u, w):
     x = x.squeeze()
     w = w.squeeze()
     u = u.squeeze()
     f = np.zeros((rx,))
-    tau = 0.1
+    tau = 0.2
     f[0] = x[0] + tau * V * np.sin(u * tau / 2) / (u * tau / 2) * np.cos(x[2] + u * tau / 2)
     f[1] = x[1] + tau * V * np.sin(u * tau / 2) / (u * tau / 2) * np.sin(x[2] + u * tau / 2)
     f[2] = x[2] + tau * u
@@ -30,19 +32,22 @@ def measurementDynamics(x, u):
 
 r=10.0
 # Obstacles params
-xs = np.array([5, -3, -9])
-ys = np.array([5, -9, 9])
-rs = np.array([3, 2, 3])
+xs = np.array([8, -3, -10])
+ys = np.array([-5, -9, 10])
+rs = np.array([2, 2, 3])
 
 def Controller(state): #Here you define your controller, whether an MPC, SMPC, CBF, PID, whatever...
     x = state[0]
     y = state[1]
     theta = state[2]
+    a = 5
     d = np.sqrt(x**2 + y**2)
     gamma = np.arctan2(y, x)
     theta_d = gamma - np.pi/2 - np.arctan(0.3 * (d-r))
-    Fx = np.cos(theta_d) 
-    Fy = np.sin(theta_d) 
+    beta = 0.3 / (1 + (0.3 * (d-r))**2)
+    theta_c = theta_d + V/(d*a) * np.sin(theta - gamma) - beta/a*V*np.cos(theta-gamma)
+    Fx = np.cos(theta_c) 
+    Fy = np.sin(theta_c) 
     u = cp.Variable((2,))
     objective = cp.Minimize(cp.quad_form(u - np.array([Fx, Fy]).squeeze(), np.diag(np.ones(2,))))
     Del_h_mat = np.full((3,2), 0.0)
@@ -57,19 +62,22 @@ def Controller(state): #Here you define your controller, whether an MPC, SMPC, C
     theta_star = np.arctan2(uStar[1],uStar[0])
     theta_error = theta_star-theta
     theta_error = np.arctan2(np.sin(theta_error), np.cos(theta_error))
-    angularVel = np.clip(10 * (theta_error), -np.pi*2, np.pi*2)
+    angularVel = np.clip(20 * (theta_error), -np.pi, np.pi)
     return angularVel
 
 
-def CostAndConstraints(Control_seq,xk2prime):
+def CostAndConstraints(Control_seq, xk2prime):
         d = np.sqrt( xk2prime[0,:] ** 2 + xk2prime[1,:] ** 2)
         gamma = np.arctan2(xk2prime[1,:], xk2prime[0,:])
-        cost_d = ( (d-r) ** 2).sum()
+        cost_d = ( (d-r) ** 2 ).sum()
         theta_d = gamma - np.pi/2 - np.arctan(0.3 * (d-r))
-        error = theta_d - xk2prime[2,:]
+        a = 5
+        beta = 0.3 / (1 + (0.3 * (d-r))**2)
+        theta_c = theta_d + V/(d*a) * np.sin(theta_d - gamma) - beta/a*V*np.cos(theta_d-gamma)
+        error = theta_c - xk2prime[2,:]
         error = np.arctan2(np.sin(error), np.cos(error))
         cost_theta = (error ** 2).sum()
-        cost = cost_d * 0 + cost_theta * 1
+        cost = cost_d * 0.05 + cost_theta * 1 
         # Bounds on the control
         #Control_violations = abs(Control_seq) > 5
         #Control_violations = np.append(Control_violations, False)
